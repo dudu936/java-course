8cb2c0cfe041645d8d883d093603078824d728ce
Eduardo Lima <eduardojunior010757@gmail.com>
2022-02-22 14:44:47 -0300

    alterações

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,7 +1,20 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello word!");
+        System.out.println("How are you?");
+        String result = scanner.nextLine().toUpperCase(Locale.ROOT);
+        if (result == "FINE"){
+            System.out.println("That is so good");
+        }else {
+            System.out.println("That is so bad");
+        }
     }
 }
